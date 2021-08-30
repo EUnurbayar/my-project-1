46be87e6a6615b12077b2300f267abb63d49a99a
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2021-08-30 14:00:21 -0500

    some changes again

--- script.js
+++ script.js
@@ -36,22 +36,29 @@ cards.forEach(el => el.addEventListener('click', event => {
 function clickHandler(event){
     event.preventDefault();
     count++;
-    console.log("clicked");
+    console.log(event);
     event.target.children[0].style.display="block";
     event.target.children[0].classList.add("active");
+    event.target.children[0].children[0].classList.add("classOfclicked")
     if(count==2){
-        const activeCards = document.getElementsByClassName("active");
-    
-        if(event.target.children[0].children[0].src!=curImgSrc){
-            setTimeout(function(){
+        const clickedCards = document.getElementsByClassName("classOfclicked");
+        const activeCards = document.getElementsByClassName("active")
+    console.log(activeCards)
+        if(activeCards[activeCards.length-2].children[0].src != activeCards[activeCards.length-1].children[0].src)  {
+           alert('noMatch')
+            setTimeout(function(){ 
+                for(const activeCard of activeCards){
+                    // activeCard.classList.remove("active")
+                    activeCard.children[0].style.display="none";
+                    activeCard.children[0].classList.remove("active");
+                 
+                  console.log(activeCard)
+                }console.log(activeCards)
                 
-                for(let activeCard of activeCards){
-                    activeCard.style.display="none";
-                    activeCard.classList.remove("active");
-                }
             }, 2000);
-            
+         
         }else{
+            alert('match')
             for(let activeCard of activeCards){
                 activeCard.classList.remove("active");
             }
@@ -72,41 +79,6 @@ function clickHandler(event){
 
 
 
-let cardsAll ={
-    pairedCardNum: 6,
-    images:[{'0': "url('https://i.pinimg.com/736x/0d/e9/85/0de98599900eec628cf6c6ce8746de92.jpg')",
-    '1':"url('https://media.giphy.com/media/JRE3AvLsSRXg360F6l/giphy.gif')",
-   '2': "url(''https://thumbs.dreamstime.com/b/cute-golden-retriever-red-scarf-isolated-yellow-background-cute-golden-retriever-red-scarf-isolated-yellow-214233088.jpg)",
-   '3': "url('https://i.pinimg.com/564x/51/91/84/519184a8e3424daa2a1520149f5ef3f1.jpg')",
-   '4': "url('https://comparic.pl/wp-content/uploads/2020/04/Black_Swam.jpg')",
-    '5': "url('http://shappa.eu/wp-content/uploads/2021/01/dogs-2-scaled.jpg')"
-    }]
-}
-//  console.log(cards)
-
-
- function render () {
-    let locArray = [];
-        while (locArray.length < (cardsAll.pairedCardNum * 2)) {
-            let loc = Math.floor(Math.random() * cards.pairedCardNum * 2);
-            if (!locArray.includes(loc)) {
-                locArray.push(loc);
-            }
-        }
-        let locArrayPairs = [];
-        for (let i = 0; i < 12; i += 2) {
-            let pair = []
-            pair.push(locArray[i]);
-            pair.push(locArray[i+1]);
-            locArrayPairs.push(pair);
-        }
-        for (let i = 0; i < this.pairedCardNum; i++) {
-            this.images[i].matchPair = locArrayPairs[i];
-        }
-    }
 
    
 
-    const reset = document.getElementsByClassName('reset')
-    // console.log(reset)
-    reset.addEventListener('click', render)
